a9140889b11239f23d507fbd9d692244890b00a4
Jesse205 <2055675594@qq.com>
2023-05-31 04:01:35 +0800

    编写代码

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,27 +2,69 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "SparklingStoreV3Documents",
-  description: "A VitePress Site",
+  lang: 'zh-CN',
+  title: '粼光商店 V3 文档',
+  description: '共建高质量应用商店',
+  base: '/SparklingStoreV3Documents/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      /*{ text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }*/
     ],
 
     sidebar: [
-      {
+      /*{
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
         ]
-      }
+      }*/
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      {
+        icon: {
+          svg: '<svg role="img" t="1672577881896" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1932" width="128" height="128"><path d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z" p-id="1933"></path></svg>'
+        },
+        link: 'https://gitee.com/sparkling-store'
+      },
+      {
+        icon: 'github',
+        link: 'https://github.com/Sparklingst'
+      },
+    ],
+
+    editLink: {
+      pattern: 'https://gitee.com/sparkling-store/SparklingStoreV3Documents/edit/master/:path',
+      text: '在 Gitee 上编辑此页面',
+    },
+
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索"
+          },
+          modal: {
+            displayDetails: '显示具详情信息',
+            resetButtonTitle: '清空内容',
+            backButtonTitle: '关闭搜索',
+            noResultsText: '未找到',
+            footer: {
+              selectText: '选择',
+              selectKeyAriaLabel: '进入',
+              navigateText: '导航',
+              navigateUpKeyAriaLabel: '向上',
+              navigateDownKeyAriaLabel: '向下',
+              closeText: '关闭',
+              closeKeyAriaLabel: '退出'
+            }
+          }
+        }
+      }
+    }
   }
 })
--- index.md
+++ index.md
@@ -2,24 +2,26 @@
 # https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
+titleTemplate: false
+
 hero:
-  name: "SparklingStoreV3Documents"
-  text: "A VitePress Site"
+  name: "粼光商店 V3 文档"
+  text: "共建高质量应用商店"
   tagline: My great project tagline
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 快速开始
+      link: /get-started
     - theme: alt
-      text: API Examples
+      text: API 文档
       link: /api-examples
 
 features:
-  - title: Feature A
+  - title: 多种平台
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
+  - title: 尊重开源
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
+  - title: 上架简单
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
 
